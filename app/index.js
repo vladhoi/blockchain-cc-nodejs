8fa8bf548ab50208aeafe8bccf976a1b86c6ef58
Vlad hoi <goyvlad2000@gmail.com>
2019-07-27 14:40:27 +0300

    mine post/request

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain')
 
 const app = express()
@@ -6,8 +7,16 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const blockchain = new Blockchain()
 
+app.use(bodyParser.json())
+
 app.get('/blocks', (req, res) => {
     res.json(blockchain.chain)
+})
+
+app.post('/mine', (req, res) => {
+    const block = blockchain.addBlock(req.body.data)
+    console.log(`New block added: ${block.toString()}`)
+    res.redirect('/blocks')
 })
 
 app.listen(HTTP_PORT, () => console.log(`Example app listening on port ${HTTP_PORT} !`))
