690c9d95a2d2a46393b218e2f55cdc2b6591944a
Vlad hoi <goyvlad2000@gmail.com>
2019-07-27 20:27:02 +0300

    Added Miner class

---
+++ app/miner.js
@@ -0,0 +1,30 @@
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
+
+class Miner {
+    constructor(blockchain, transactionPool, wallet, p2pServer) {
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.p2pServer = p2pServer;
+    }
+
+    mine() {
+        const validTransactions = this.transactionPool.validTransactions();
+        validTransactions.push(
+            Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
+        );
+        
+        const block = this.blockchain.addBlock(validTransactions);
+        // synchronize the chains to p2p server
+        this.p2pServer.syncChains();
+        // clear the transaction pool
+        this.transactionPool.clear();
+        // broadcast to every miner
+        this.p2pServer.broadcastClearTransactions();
+
+        return block;
+    }
+}
+
+module.exports = Miner;
--- dev-test.js
+++
@@ -1,5 +0,0 @@
-const Wallet = require('./wallet')
-
-const wallet = new Wallet();
-
-console.log(wallet.toString())
